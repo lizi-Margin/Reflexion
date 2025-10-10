5b352b8d548b6f5355539e3d0aa73b29a139af00
hulc <3314986382@qq.com>
2025-10-10 20:22:25 +0800

    offline_play.py for general track, modified ver.

--- offline_play.py
+++ offline_play.py
@@ -10,34 +10,41 @@ from corleone.agents.track2.track2_router import create_track2_agent
 import sys
 import io
 import os
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+# sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
+try:
+  from uhtk.print_pack import print_bold_green as print
+except: pass
 
 
 # env_ids = ["SecretMafia-v0-train"]
 # env_ids = ["Codenames-v0-train", "ColonelBlotto-v0-train", "ThreePlayerIPD-v0-train"]
 # initialize the environment
-env_id = "Codenames-v0-train"
+env_id = "Codenames-v0-train"; npc_num = 3  # 2v2 
 agents = {
     0: create_track2_agent(model_name='test', api_model_spec='qwen3-8b', env_name=env_id, enable_logging=False),
-    1: create_track2_agent(model_name='bsl1', api_model_spec='qwen3-8b', env_name=env_id, enable_logging=False),
-    2: create_track2_agent(model_name='bsl2', api_model_spec='qwen3-8b', env_name=env_id, enable_logging=False),
-    3: create_track2_agent(model_name='bsl3', api_model_spec='qwen3-8b', env_name=env_id, enable_logging=False),
-    4: create_track2_agent(model_name='bsl4', api_model_spec='qwen3-8b', env_name=env_id, enable_logging=False),
-    5: create_track2_agent(model_name='bsl5', api_model_spec='qwen3-8b', env_name=env_id, enable_logging=False),
 }
+for i in range(1, npc_num+1):
+    agents[i] = create_track2_agent(model_name=f'bsl{i}', api_model_spec='qwen3-8b', env_name=env_id, enable_logging=False)
 
-
+print(f"Playing {env_id} with {npc_num} NPCs")
+print("start make env, env_id:", env_id)
 env = ta.make(env_id=env_id)
 env.reset(num_players=len(agents))
 
 # main game loop
 done = False 
+step = 0
 while not done:
+  step += 1
+  print(f"Step {step} start! ------------------------------------------------>")
   player_id, observation = env.get_observation()
+  print(f"Player {player_id} observation: {observation}")
   action = agents[player_id](observation)
+  print(f"Player {player_id} action: {action}")
   done, step_info = env.step(action=action)
+  print(f"Step {step} end! ------------------------------------------------>")
 rewards, game_info = env.close()
 
 print(f"Rewards: {rewards}")
